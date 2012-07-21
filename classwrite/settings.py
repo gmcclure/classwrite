29f886595b5482c2fea6965c248f4f0ca563f3e0
Greg McClure <gmcclure@gmail.com>
2012-07-21 00:25:14 -0700

    Added South.

--- classwrite/settings.py
+++ classwrite/settings.py
@@ -122,9 +122,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'django.contrib.markup',
+    'south',
     'social_auth',
     'front',
     'account',
